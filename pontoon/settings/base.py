918e0007c9fedd1abecfc522a9cf627418bd6c9c
Florian Gilcher <florian.gilcher@asquera.de>
2019-05-27 21:07:57 +0200

    Use GitHub instead of FXA

--- pontoon/settings/base.py
+++ pontoon/settings/base.py
@@ -146,7 +146,7 @@ INSTALLED_APPS = (
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'allauth.socialaccount.providers.fxa',
+    'allauth.socialaccount.providers.github',
     'notifications',
     'graphene_django',
     'webpack_loader',
@@ -807,7 +807,6 @@ CORS_URLS_REGEX = r'^/(pontoon\.js|graphql/?)$'
 SOCIALACCOUNT_ENABLED = True
 SOCIALACCOUNT_ADAPTER = 'pontoon.base.adapter.PontoonSocialAdapter'
 
-
 def account_username(user):
     return user.name_or_email
 
@@ -816,22 +815,6 @@ ACCOUNT_AUTHENTICATED_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_USER_DISPLAY = account_username
-
-# Firefox Accounts
-FXA_CLIENT_ID = os.environ.get('FXA_CLIENT_ID', '')
-FXA_SECRET_KEY = os.environ.get('FXA_SECRET_KEY', '')
-FXA_OAUTH_ENDPOINT = os.environ.get('FXA_OAUTH_ENDPOINT', '')
-FXA_PROFILE_ENDPOINT = os.environ.get('FXA_PROFILE_ENDPOINT', '')
-FXA_SCOPE = ['profile:uid', 'profile:display_name', 'profile:email']
-
-# All settings related to the AllAuth
-SOCIALACCOUNT_PROVIDERS = {
-    'fxa': {
-        'SCOPE': FXA_SCOPE,
-        'OAUTH_ENDPOINT': FXA_OAUTH_ENDPOINT,
-        'PROFILE_ENDPOINT': FXA_PROFILE_ENDPOINT,
-    }
-}
 
 # Defined all trusted origins that will be returned in pontoon.js file.
 if os.environ.get('JS_TRUSTED_ORIGINS'):
